deb43644923f1665809e112169a342f93cf68df3
Abidhussainqazi <125920304+Abidhussainqazi@users.noreply.github.com>
2023-04-25 20:26:55 +0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,14 +1,15 @@
 //Write a function that takes an array of numbers as input and returns the sum of all even numbers in the array.
 
-function sumEvenNumbers(arr) {
+function sumEvenNumbers(numbers) {
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      sum += arr[i];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      sum += numbers[i];
     }
   }
   return sum;
 }
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const evenSum = sumEvenNumbers(numbers);
+
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenSum = sumEvenNumbers(input);
 document.write("The sum of even numbers in the array is: " + evenSum);
