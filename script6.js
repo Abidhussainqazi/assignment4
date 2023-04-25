72b719250b9cdd7b466d9d28b559cccd15f2c9ae
Abidhussainqazi <125920304+Abidhussainqazi@users.noreply.github.com>
2023-04-25 20:44:49 +0500

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,11 +1,12 @@
 //Write a function that takes an array of objects as input and returns an array of all the values for a specified key in each object.
-function getValuesByKey(arr, key) {
+function getValuesByKey(people, key) {
   const values = [];
-  for (let i = 0; i < arr.length; i++) {
-    values.push(arr[i][key]);
+  for (let i = 0; i < people.length; i++) {
+    values.push(people[i][key]);
   }
   return values;
 }
-const arr = [{ name: "John", age: 30 }, { name: "Mary", age: 25 }, { name: "Peter", age: 35 }];
-const values = getValuesByKey(arr, "name");
+
+const people = [{ name: "John", age: 30 }, { name: "Mary", age: 25 }, { name: "Peter", age: 35 }];
+const values = getValuesByKey(people, "name");
 document.write("The values for the 'name' key are: " + values);
