7be8e16a0eb9f017c77d1d4cb89fd43eb007a70a
Abidhussainqazi <125920304+Abidhussainqazi@users.noreply.github.com>
2023-04-25 20:31:43 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,14 +1,14 @@
 //Write a function that takes a string as input and returns the number of vowels in the string.
-function countVowels(str) {
+function countVowels(string) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i].toLowerCase())) {
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i].toLowerCase())) {
       count++;
     }
   }
   return count;
 }
-const str = "hello world";
-const vowelCount = countVowels(str);
+const string = "hello world";
+const vowelCount = countVowels(string);
 document.write("The number of vowels in the string is: " + vowelCount);
