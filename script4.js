828ec882f46f894bb9d878d4466bdf9c3823c989
Abidhussainqazi <125920304+Abidhussainqazi@users.noreply.github.com>
2023-04-25 20:37:25 +0500

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,12 +1,13 @@
 //Write a function that takes an array of numbers as input and returns the average of all the numbers in the array.
-function findAverage(arr) {
+function findAverage(numberArray) {
   let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+  for (let i = 0; i < numberArray.length; i++) {
+    sum += numberArray[i];
   }
-  const avg = sum / arr.length;
+  const avg = sum / numberArray.length;
   return avg;
 }
+
 const nums = [1, 2, 3, 4, 5];
 const avg = findAverage(nums);
 document.write("The average of the numbers in the array is: " + avg);
