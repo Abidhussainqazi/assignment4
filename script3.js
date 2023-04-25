b5c0ac741f5c1ea3bc74d234e00cc1df19d692f2
Abidhussainqazi <125920304+Abidhussainqazi@users.noreply.github.com>
2023-04-25 20:35:06 +0500

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,13 +1,14 @@
 //Write a function that takes an array of strings as input and returns the length of the longest string in the array.
-function findLongestLength(arr) {
+function findLongestLength(stringArray) {
   let longestLength = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length > longestLength) {
-      longestLength = arr[i].length;
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i].length > longestLength) {
+      longestLength = stringArray[i].length;
     }
   }
   return longestLength;
 }
+
 const strings = ["apple", "banana", "pear", "grapefruit"];
 const longestLength = findLongestLength(strings);
 document.write("The length of the longest string in the array is: " + longestLength);
